2e236b366e94f5e407d18154b8658a1a54729313
Bartosz <49102695+Barthosz@users.noreply.github.com>
2023-09-26 17:49:23 +0200

    add import

---
+++ script.js
@@ -0,0 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
